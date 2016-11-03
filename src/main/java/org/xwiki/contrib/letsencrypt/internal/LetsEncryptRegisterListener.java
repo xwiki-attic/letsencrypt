bdc54c16c070f396fd3ea4294d6ffa77207e7bfe
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-11-03 17:51:16 +0100

    LENCRYPT-2: Let's Encrypt module overwite javax.net.ssl.trustStore with the default store

--- src/main/java/org/xwiki/contrib/letsencrypt/internal/LetsEncryptRegisterListener.java
+++ src/main/java/org/xwiki/contrib/letsencrypt/internal/LetsEncryptRegisterListener.java
@@ -20,17 +20,26 @@
 package org.xwiki.contrib.letsencrypt.internal;
 
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 
@@ -56,6 +65,10 @@ public class LetsEncryptRegisterListener extends AbstractEventListener implement
      */
     public static final String NAME = "letsencrypt";
 
+    private static final String CERTIFICATE_ALIAS = "DSTRootCAX3";
+
+    private static final String CERTIFICATE_FILE = CERTIFICATE_ALIAS + ".der";
+
     @Inject
     private Logger logger;
 
@@ -71,28 +84,108 @@ public class LetsEncryptRegisterListener extends AbstractEventListener implement
     public void initialize() throws InitializationException
     {
         try {
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            Path ksPath = Paths.get(System.getProperty("java.home"), "lib", "security", "cacerts");
-            keyStore.load(Files.newInputStream(ksPath), "changeit".toCharArray());
+            String keyStore = System.getProperty("javax.net.ssl.keyStore");
 
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            try (InputStream caInput = new BufferedInputStream(
-                // this files is shipped with the application
-                getClass().getResourceAsStream("DSTRootCAX3.der"))) {
-                Certificate crt = cf.generateCertificate(caInput);
-                this.logger.info("Added Cert for [{}]", ((X509Certificate) crt).getSubjectDN());
-
-                keyStore.setCertificateEntry("DSTRootCAX3", crt);
+            SSLContext sslContext;
+            if (keyStore != null) {
+                sslContext = updateCustomTrustStore(keyStore);
+            } else {
+                sslContext = updateOracleTrustStore();
             }
 
-            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            tmf.init(keyStore);
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, tmf.getTrustManagers(), null);
-            SSLContext.setDefault(sslContext);
+            if (sslContext != null) {
+                SSLContext.setDefault(sslContext);
+            }
         } catch (Exception e) {
             throw new InitializationException("Failed to regsiter Let's Encryp certificate", e);
         }
+    }
+
+    private Certificate getCertificate() throws CertificateException, IOException
+    {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        try (InputStream caInput = new BufferedInputStream(
+            // this files is shipped with the application
+            getClass().getResourceAsStream(CERTIFICATE_FILE))) {
+            return cf.generateCertificate(caInput);
+        }
+    }
+
+    private SSLContext getSSLContext() throws NoSuchAlgorithmException
+    {
+        return SSLContext.getInstance("TLS");
+    }
+
+    private SSLContext updateOracleTrustStore()
+        throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException, KeyManagementException
+    {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        Path ksPath = Paths.get(System.getProperty("java.home"), "lib", "security", "cacerts");
+        keyStore.load(Files.newInputStream(ksPath), "changeit".toCharArray());
+
+        // Make sure the certificate is not already registered
+        if (keyStore.getCertificate(CERTIFICATE_ALIAS) == null) {
+            // Add Let's Encrypt certificate
+            Certificate certificate = getCertificate();
+            this.logger.info("Added certificate [{}] in default [{}]", ((X509Certificate) certificate).getSubjectDN(),
+                ksPath);
+            keyStore.setCertificateEntry(CERTIFICATE_ALIAS, certificate);
+
+            TrustManagerFactory trustManagerFactory =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+
+            SSLContext sslContext = getSSLContext();
+            sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+            return sslContext;
+        }
+
+        return null;
+    }
+
+    private SSLContext updateCustomTrustStore(String keyStoreString) throws KeyStoreException, NoSuchAlgorithmException,
+        CertificateException, IOException, KeyManagementException, UnrecoverableKeyException
+    {
+        String keyStoreTypeString = System.getProperty("javax.net.ssl.keyStoreType", KeyStore.getDefaultType());
+        String keyStorePasswordString = System.getProperty("javax.net.ssl.keyStorePassword", "");
+
+        KeyManager[] kms = null;
+        if (keyStoreString != null && !keyStoreString.equals("NONE")) {
+            KeyManagerFactory keyManagerFactory =
+                KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            KeyStore keyStore = KeyStore.getInstance(keyStoreTypeString);
+
+            try (FileInputStream fs = new FileInputStream(keyStoreString)) {
+                keyStore.load(fs, keyStorePasswordString.toCharArray());
+            }
+
+            // Make sure the certificate is not already registered
+            if (keyStore.getCertificate(CERTIFICATE_ALIAS) != null) {
+                // Add Let's Encrypt certificate
+                Certificate certificate = getCertificate();
+                this.logger.info("Added certificate [{}] in custom [{}]",
+                    ((X509Certificate) certificate).getSubjectDN(), keyStoreString);
+                keyStore.setCertificateEntry(CERTIFICATE_ALIAS, certificate);
+
+                char[] password;
+                if (keyStorePasswordString.length() > 0) {
+                    password = keyStorePasswordString.toCharArray();
+                } else {
+                    password = null;
+                }
+                keyManagerFactory.init(keyStore, password);
+                kms = keyManagerFactory.getKeyManagers();
+
+                SSLContext sslContext = getSSLContext();
+                sslContext.init(kms, null, null);
+
+                return sslContext;
+            }
+        }
+
+        return null;
     }
 
     @Override
